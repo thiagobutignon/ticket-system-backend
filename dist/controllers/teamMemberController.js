a6a7070b70fdc49bfd17a6bc88868adf31a90697
thiagobutignon <thiago.butignon@townsq.com.br>
2025-02-27 16:30:43 -0300

    refactor: create the route to get team members

---
+++ dist/controllers/teamMemberController.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getTeamMembers = void 0;
+const getTeamMembers = () => {
+    return [
+        { id: '1', name: 'Alice', role: 'Developer' },
+        { id: '2', name: 'Bob', role: 'Developer' },
+        { id: '3', name: 'Charlie', role: 'Designer' },
+    ];
+};
+exports.getTeamMembers = getTeamMembers;
+//# sourceMappingURL=teamMemberController.js.map
